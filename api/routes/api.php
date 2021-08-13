ee9c16bf1344e3959ad64490fed9ef642daff114
Halí V <halivelazquez@outlook.com>
2021-08-12 22:31:50 -0500

    Update sanctum

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => [
+        'auth:api',
+        'auth:sanctum',
+    ]
+], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
